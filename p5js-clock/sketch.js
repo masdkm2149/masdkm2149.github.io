c59f3325b1dbe2940255e031a3b0dc4a7c79ae2b
masdkm2149 <36574394+masdkm2149@users.noreply.github.com>
2022-06-10 10:56:40 -0400

    Update sketch.js

--- p5js-clock/sketch.js
+++ p5js-clock/sketch.js
@@ -10,7 +10,7 @@ h = window.innerHeight;
 document.querySelector("body").style.setProperty('transform', 'scale('+ w*.0005 +')');
 	console.log(w + " " + h);
 	  
-	  if (w < 600) {createCanvas(w*3, h); document.querySelector("body").style.setProperty('left', 'calc(-30% + 20px);');}
+	  if (w < 600) {createCanvas(w*3, h); document.querySelector("body").style.setProperty('left', 'calc(-30% + 20px)');}
 	else{
 	createCanvas(w*2, h);
 		document.querySelector("body").style.setProperty('left','calc(-60% + 250px)');

5bb2e41d565fa8b76c33e6c6afe90212283e49ac
masdkm2149 <36574394+masdkm2149@users.noreply.github.com>
2023-06-14 09:02:36 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,15 @@ function setup() {
 
 function draw() {
           colorMode(HSB);
-    background(random(1,360), random(20,100), random(10,100));
+        let r1 = (1,360)
+        let r2 = (20,100)
+        let r3 = (10,100)
+        background(random(r1), random(r2), random(r3));
+      textSize(40);
+            fill(random(r1*.75), random(r2*.82), random(r3/3*2.87));
+      text("Daniel Roswadowsky", windowWidth/2, windowHeight/2);
+      textSize(18);
+    text("Interactive Design", windowWidth/2, windowHeight/2 + 50);
 }
   function mouseWheel(event) {
     scrollDelta = event.delta;
@@ -13,12 +21,29 @@ function draw() {
     clear();
    
       if (scrollDelta > 0) {
-      colorMode(HSB);
-  background(random(1,360), random(20,100), random(10,100));
+        colorMode(HSB);
+        let r1 = (1,360)
+        let r2 = (20,100)
+        let r3 = (10,100)
+        background(random(r1), random(r2), random(r3));
+      textSize(40);
+            fill(random(r1*.75), random(r2*.82), random(r3/3*2.87));
+      text("Daniel Roswadowsky", windowWidth/2, windowHeight/2);
+      textSize(18);
+    text("Interactive Design", windowWidth/2, windowHeight/2 + 50);
       } 
     else {
         colorMode(HSB);
-        background(random(1,360), random(20,100), random(10,100));
+        let r1 = (1,360)
+        let r2 = (20,100)
+        let r3 = (10,100)
+        background(random(r1), random(r2), random(r3));
+      textSize(40);
+            fill(random(r1*.75), random(r2*.82), random(r3/3*2.87));
+      text("Daniel Roswadowsky", windowWidth/2, windowHeight/2);
+      textSize(18);
+    text("Interactive Design", windowWidth/2, windowHeight/2 + 50);
+
     }
 
 }

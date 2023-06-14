ccfbc4655c3a10a7a4df463a7be51e2c65419891
masdkm2149 <36574394+masdkm2149@users.noreply.github.com>
2023-06-14 14:44:18 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+        colorMode(HSB);
+          let r1 = (1,360)
+        let r2 = (20,100)
+        let r3 = (10,100)
+        background(random(r1), random(r2), random(r3));
+      textSize(40);
+            fill(random(r1*.75), random(r2*.82), random(r3/3*2.87));
+      text("Daniel Roswadowsky", windowWidth/2, windowHeight/2);
+      textSize(18);
+    text("Interactive Design", windowWidth/2, windowHeight/2 + 50);
 }
 
 
@@ -12,7 +22,7 @@ function draw() {
     clear();
    
       if (scrollDelta > 0) {
-        colorMode(HSB);
+      colorMode(HSB);    
         let r1 = (1,360)
         let r2 = (20,100)
         let r3 = (10,100)

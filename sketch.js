c55e342381a96fe426e60d141a4d4150f0b11229
masdkm2149 <36574394+masdkm2149@users.noreply.github.com>
2023-06-14 08:22:08 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+
+function draw() {
+}
+  function mouseWheel(event) {
+    scrollDelta = event.delta;
+   
+    clear();
+   
+      if (scrollDelta > 0) {
+      colorMode(HSB);
+  background(random(1,360), random(20,100), random(10,100));
+      } 
+    else {
+        colorMode(HSB);
+        background(random(1,360), random(20,100), random(10,100));
+    }
+
+}

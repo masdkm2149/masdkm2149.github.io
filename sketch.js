9a2c0fc04789a1a25930a583f59ca91f83481d6f
masdkm2149 <36574394+masdkm2149@users.noreply.github.com>
2023-06-14 09:07:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,16 +4,7 @@ function setup() {
 
 
 function draw() {
-          colorMode(HSB);
-        let r1 = (1,360)
-        let r2 = (20,100)
-        let r3 = (10,100)
-        background(random(r1), random(r2), random(r3));
-      textSize(40);
-            fill(random(r1*.75), random(r2*.82), random(r3/3*2.87));
-      text("Daniel Roswadowsky", windowWidth/2, windowHeight/2);
-      textSize(18);
-    text("Interactive Design", windowWidth/2, windowHeight/2 + 50);
+
 }
   function mouseWheel(event) {
     scrollDelta = event.delta;

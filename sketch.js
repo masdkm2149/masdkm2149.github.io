59f497d662a6b33c19d9796d0fefeadb01bf1578
masdkm2149 <36574394+masdkm2149@users.noreply.github.com>
2023-06-14 22:15:36 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup() {
 
-    createCanvas(windowWidth, windowHeight);
+   // createCanvas(windowWidth, windowHeight);
      const rx = (0.8, 2)
         colorMode(HSB);
           let r1 = (1,360)
@@ -161,7 +161,7 @@ $( document ).ready(function() {
 });
 
   function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+//  resizeCanvas(windowWidth, windowHeight);
            const rx = (0.8, 2)
         colorMode(HSB);
           let r1 = (1,360)

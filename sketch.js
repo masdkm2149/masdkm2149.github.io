5af481e653a23d6874b7566673e4649ebf7fe582
masdkm2149 <36574394+masdkm2149@users.noreply.github.com>
2023-06-17 20:17:56 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,4 @@
 function setup() {
-
-
      const rx = (0.8, 2)
         colorMode(HSB);
           let r1 = (1,360)
@@ -9,12 +7,9 @@ function setup() {
         var rr1 = random(r1)
         var rr2 = random(r2)
         var rr3 = random(r3)
-        var rr = random(rx)
-//        background(rr1, rr2, rr3);
             document.querySelector(':root').style.setProperty('--bg_hue', rr1);
             document.querySelector(':root').style.setProperty('--bg_brightness', rr2 + '%');
             document.querySelector(':root').style.setProperty('--bg_saturation', rr3 + '%');
-      textSize(40);
     if (rr3 > 50) {
             xo = random(1, rr3*0.85)
         document.querySelector(':root').style.setProperty('--txt_saturation', xo + '%');
@@ -61,7 +56,7 @@ function setup() {
             xi = random(0, 270)
         document.querySelector(':root').style.setProperty('--txt_hue', xi);
       }
- 
+ //       textSize(40);
  //     text("Daniel Roswadowsky", windowWidth/3, windowHeight/2);
  //     textSize(18);
  //   text("Interactive Design", windowWidth/3, windowHeight/2 + 50);   
@@ -144,9 +139,9 @@ resizeCanvas(windowWidth, windowHeight);
 function draw() {
 clear();
 ellipse(mouseX, mouseY, 80, 80);
-  colorMode(HSB)
-  fill(0, 100, 100)
-  noStroke()
+  colorMode(HSB);
+  fill(xi, xo, xy);
+  noStroke();
 }
   function mouseWheel(event) {
     scrollDelta = event.delta;

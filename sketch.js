80d268af76b2afd852576944d5bfa68a2cf78cdb
masdkm2149 <36574394+masdkm2149@users.noreply.github.com>
2023-10-05 09:47:56 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,50 +26,50 @@ function setup() {
             document.querySelector(':root').style.setProperty('--bg_brightness', rr2 + '%');
             document.querySelector(':root').style.setProperty('--bg_saturation', rr3 + '%');
     if (rr3 > 50) {
-            xo = random(1, rr3*0.85)
-        document.querySelector(':root').style.setProperty('--txt_saturation', xo + '%');
+            sat = random(1, rr3*0.85)
+        document.querySelector(':root').style.setProperty('--txt_saturation', sat + '%');
     }       
     else if (rr3 < 50) {
-            xo = random(rr3*1.15, 100)
-        document.querySelector(':root').style.setProperty('--txt_saturation', xo + '%');
+            sat = random(rr3*1.15, 100)
+        document.querySelector(':root').style.setProperty('--txt_saturation', sat + '%');
     }       
    
    if (rr2 > 50) {
-            xy = random(1, rr2*0.7)
-        document.querySelector(':root').style.setProperty('--txt_brightness', xy + '%');
+            luma = random(1, rr2*0.7)
+        document.querySelector(':root').style.setProperty('--txt_brightness', luma + '%');
     }       
     else if (rr2 < 50) {
-            xy = random(rr2*1.3, 100)
-        document.querySelector(':root').style.setProperty('--txt_brightness', xy + '%');
+            luma = random(rr2*1.3, 100)
+        document.querySelector(':root').style.setProperty('--txt_brightness', luma + '%');
     }       
    
    if  (rr1 < 45) {
-                    xi = random(90, 360)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+                    hue = random(90, 360)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
      }
   else if (rr1 > 90) {
-            xi = random(0, 45)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 45)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
    else if (rr1 > 135) {
-            xi = random(0, 90)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 90)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
     else if (rr1 > 180) {
-            xi = random(0, 135)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 135)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
     else if (rr1 > 225) {
-            xi = random(0, 180)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 180)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
          else if (rr1 > 270) {
-            xi = random(0, 225)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 225)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
          else if (rr1 > 315) {
-            xi = random(0, 270)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 270)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
  //       textSize(40);
  //     text("Daniel Roswadowsky", windowWidth/3, windowHeight/2);
@@ -107,50 +107,50 @@ $( document ).ready(function() {
             document.querySelector(':root').style.setProperty('--bg_saturation', rr3 + '%');
       textSize(40);
     if (rr3 > 50) {
-            xo = random(1, rr3*0.85)
-        document.querySelector(':root').style.setProperty('--txt_saturation', xo + '%');
+            sat = random(1, rr3*0.85)
+        document.querySelector(':root').style.setProperty('--txt_saturation', sat + '%');
     }       
     else if (rr3 < 50) {
-            xo = random(rr3*1.15, 100)
-        document.querySelector(':root').style.setProperty('--txt_saturation', xo + '%');
+            sat = random(rr3*1.15, 100)
+        document.querySelector(':root').style.setProperty('--txt_saturation', sat + '%');
     }       
    
    if (rr2 > 50) {
-            xy = random(1, rr2*0.7)
-        document.querySelector(':root').style.setProperty('--txt_brightness', xy + '%');
+            luma = random(1, rr2*0.7)
+        document.querySelector(':root').style.setProperty('--txt_brightness', luma + '%');
     }       
     else if (rr2 < 50) {
-            xy = random(rr2*1.3, 100)
-        document.querySelector(':root').style.setProperty('--txt_brightness', xy + '%');
+            luma = random(rr2*1.3, 100)
+        document.querySelector(':root').style.setProperty('--txt_brightness', luma + '%');
     }       
    
    if  (rr1 < 45) {
-                    xi = random(90, 360)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+                    hue = random(90, 360)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
      }
   else if (rr1 > 90) {
-            xi = random(0, 45)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 45)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
    else if (rr1 > 135) {
-            xi = random(0, 90)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 90)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
     else if (rr1 > 180) {
-            xi = random(0, 135)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 135)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
     else if (rr1 > 225) {
-            xi = random(0, 180)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 180)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
          else if (rr1 > 270) {
-            xi = random(0, 225)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 225)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
          else if (rr1 > 315) {
-            xi = random(0, 270)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 270)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
     //  text("Daniel Roswadowsky", windowWidth/3, windowHeight/2);
     //  textSize(18);
@@ -160,7 +160,7 @@ function draw() {
 clear();
 ellipse(mouseX, mouseY, 80, 80);
   colorMode(HSB);
-  fill(xi, xo, xy);
+  fill(hue, sat, luma);
   noStroke();
      document.querySelector(':root').style.setProperty('--mousex', mouseX + 'px');
      document.querySelector(':root').style.setProperty('--mousey', mouseY + 'px');
@@ -185,50 +185,50 @@ ellipse(mouseX, mouseY, 80, 80);
             document.querySelector(':root').style.setProperty('--bg_saturation', rr3 + '%');
       textSize(40);  
     if (rr3 > 50) {
-            xo = random(1, rr3*0.85)
-        document.querySelector(':root').style.setProperty('--txt_saturation', xo + '%');
+            sat = random(1, rr3*0.85)
+        document.querySelector(':root').style.setProperty('--txt_saturation', sat + '%');
     }       
     else if (rr3 < 50) {
-            xo = random(rr3*1.15, 100)
-        document.querySelector(':root').style.setProperty('--txt_saturation', xo + '%');
+            sat = random(rr3*1.15, 100)
+        document.querySelector(':root').style.setProperty('--txt_saturation', sat + '%');
     }       
    
    if (rr2 > 50) {
-            xy = random(1, rr2*0.7)
-        document.querySelector(':root').style.setProperty('--txt_brightness', xy + '%');
+            luma = random(1, rr2*0.7)
+        document.querySelector(':root').style.setProperty('--txt_brightness', luma + '%');
     }       
     else if (rr2 < 50) {
-            xy = random(rr2*1.3, 100)
-        document.querySelector(':root').style.setProperty('--txt_brightness', xy + '%');
+            luma = random(rr2*1.3, 100)
+        document.querySelector(':root').style.setProperty('--txt_brightness', luma + '%');
     }       
    
    if  (rr1 < 45) {
-                    xi = random(90, 360)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+                    hue = random(90, 360)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
      }
   else if (rr1 > 90) {
-            xi = random(0, 45)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 45)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
    else if (rr1 > 135) {
-            xi = random(0, 90)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 90)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
     else if (rr1 > 180) {
-            xi = random(0, 135)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 135)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
     else if (rr1 > 225) {
-            xi = random(0, 180)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 180)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
          else if (rr1 > 270) {
-            xi = random(0, 225)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 225)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
          else if (rr1 > 315) {
-            xi = random(0, 270)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 270)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
 //      text("Daniel Roswadowsky", windowWidth/3, windowHeight/2);
   //    textSize(18);
@@ -249,50 +249,50 @@ ellipse(mouseX, mouseY, 80, 80);
             document.querySelector(':root').style.setProperty('--bg_saturation', rr3 + '%');
       textSize(40);
     if (rr3 > 50) {
-            xo = random(1, rr3*0.85)
-        document.querySelector(':root').style.setProperty('--txt_saturation', xo + '%');
+            sat = random(1, rr3*0.85)
+        document.querySelector(':root').style.setProperty('--txt_saturation', sat + '%');
     }       
     else if (rr3 < 50) {
-            xo = random(rr3*1.15, 100)
-        document.querySelector(':root').style.setProperty('--txt_saturation', xo + '%');
+            sat = random(rr3*1.15, 100)
+        document.querySelector(':root').style.setProperty('--txt_saturation', sat + '%');
     }       
    
    if (rr2 > 50) {
-            xy = random(1, rr2*0.7)
-        document.querySelector(':root').style.setProperty('--txt_brightness', xy + '%');
+            luma = random(1, rr2*0.7)
+        document.querySelector(':root').style.setProperty('--txt_brightness', luma + '%');
     }       
     else if (rr2 < 50) {
-            xy = random(rr2*1.3, 100)
-        document.querySelector(':root').style.setProperty('--txt_brightness', xy + '%');
+            luma = random(rr2*1.3, 100)
+        document.querySelector(':root').style.setProperty('--txt_brightness', luma + '%');
     }       
    
    if  (rr1 < 45) {
-                    xi = random(90, 360)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+                    hue = random(90, 360)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
      }
   else if (rr1 > 90) {
-            xi = random(0, 45)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 45)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
    else if (rr1 > 135) {
-            xi = random(0, 90)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 90)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
     else if (rr1 > 180) {
-            xi = random(0, 135)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 135)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
     else if (rr1 > 225) {
-            xi = random(0, 180)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 180)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
          else if (rr1 > 270) {
-            xi = random(0, 225)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 225)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
          else if (rr1 > 315) {
-            xi = random(0, 270)
-        document.querySelector(':root').style.setProperty('--txt_hue', xi);
+            hue = random(0, 270)
+        document.querySelector(':root').style.setProperty('--txt_hue', hue);
       }
 // text("Daniel Roswadowsky", windowWidth/3, windowHeight/2);
   //    textSize(18);

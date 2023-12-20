38b725b0714b6cc510eac123ced1d5ba772fcb44
masdkm2149 <36574394+masdkm2149@users.noreply.github.com>
2023-12-20 16:25:59 -0500

    Create script.js

---
+++ soundexp/script.js
@@ -0,0 +1,29 @@
+let dsharp;
+let fnat;
+function preload() {
+  soundFormats('wav');
+  dsharp = loadSound('https://designedbydan.art/assets/D%23wav.wav');
+  dsharp.playMode('sustain');
+      dsharp.loop = 0;
+  fnat = loadSound('https://designedbydan.art/assets/Fwav.wav');
+  fnat.playMode('sustain');
+    fnat.loop = 0;
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+
+  function draw() {
+  let x = width - width*0.052
+  let y = height - height*0.054
+  let a = map(mouseX, 0, width, width*0.052, x);
+  let b = map(mouseY, 0, height, height*0.054, y);
+    background(220);
+      circle(a, b ,width*0.1);
+
+      if (mouseX < 50) { dsharp.play();}
+    
+      if (mouseY < 200) { fnat.play();}
+}

19371d4a7836e5ef7754aeae8690c7f20b81a0c7
masdkm2149 <36574394+masdkm2149@users.noreply.github.com>
2023-12-20 18:30:10 -0500

    Update script.js

--- soundexp/script.js
+++ soundexp/script.js
@@ -1,15 +1,24 @@
 let dsharp;
 let fnat;
+let gnat;
+let anat;
 function preload() {
   soundFormats('mp3');
   dsharp = loadSound('https://designedbydan.art/soundexp/sounds/dsharp.mp3');
   dsharp.playMode('sustain');
-      dsharp.loop = 0;
+    dsharp.loop = 1;
   fnat = loadSound('https://designedbydan.art/soundexp/sounds/fnat.mp3');
   fnat.playMode('sustain');
-    fnat.loop = 0;
-    gnat = loadSound('https://designedbydan.art/soundexp/sounds/gnat.mp3');
+    fnat.loop = 1;
+  gnat = loadSound('https://designedbydan.art/soundexp/sounds/gnat.mp3');
   gnat.playMode('sustain');
+    gnat.loop = 1;
+  anat = loadSound('https://designedbydan.art/soundexp/sounds/anat.mp3');
+  anat.playMode('sustain');
+    anat.loop = 1;
+  bnat = loadSound('https://designedbydan.art/soundexp/sounds/bnat.mp3');
+  bnat.playMode('sustain');
+    bnat.loop = 1;
 }
 
 function setup() {
@@ -25,11 +34,9 @@ function setup() {
     background(220);
       circle(a, b ,width*0.1);
 
-      if (mouseX < 50) { 
-        console.log(dsharp.isPlaying())
-        if ( dsharp.isPlaying() == false ) {
-        dsharp.play();}
-      }
-      if (mouseX < 150) { if ( fnat.isPlaying() == false ) { fnat.play();}}
-          if (mouseX < 250) { if ( gnat.isPlaying() == false ) { gnat.play();}}
+    if (mouseX < width*0.1) { if ( dsharp.isPlaying() == false ) { dsharp.play();}}
+      if (mouseX < width*0.2) { if ( fnat.isPlaying() == false ) { fnat.play();}}
+          if (mouseX < width*0.3) { if ( gnat.isPlaying() == false ) { gnat.play();}}
+              if (mouseX < width*0.4) { if ( anat.isPlaying() == false ) { anat.play();}}
+                  if (mouseX < width*0.5) { if ( bnat.isPlaying() == false ) { bnat.play();}}
 }

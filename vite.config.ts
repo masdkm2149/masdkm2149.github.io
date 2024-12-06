d909e0027c6856f40d6b5a67071e0e6f4fc32544
masdkm2149 <36574394+masdkm2149@users.noreply.github.com>
2024-12-05 21:17:07 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
-  base: "https://designedbydan.art",
+  base: "https://designedbydan.art/",
   plugins: [
-})
+    react(),
+  ],
